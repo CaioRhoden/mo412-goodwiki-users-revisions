477e1c51fcaf78375a2977cd3e38bb3faa87c777
Daniel Gardin <gardin.dan@gmail.com>
2025-11-28 11:17:16 -0300

    data_processing files done

--- data_processing/get_revision.py
+++ data_processing/get_revision.py
@@ -1,19 +1,27 @@
-from turtle import st
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
 import polars as pl
 import requests
-import argparse
-import datetime
 
-DATA_PATH = "../data/09_04_2023_v1.parquet"
+from tyro import cli
+
+DATA_PATH = "../data/goodwiki.parquet"
 SAVING_PATH = "../data"
 URL = "https://en.wikipedia.org/w/api.php"
 HEADERS = {
     "User-Agent": "GoodWikiUsersRevisions/1.0 (caior@example.com)"
 }
 
-def make_request(title, rvstart="2023-01-01T00:00:00Z", rvend="2023-09-04T00:00:00Z") -> list[dict]:
+def make_request(
+    title: str,
+    rvstart: str = "2023-01-01T00:00:00Z",
+    rvend: str = "2023-09-04T00:00:00Z"
+) -> list[dict[str, Any]]:
     S = requests.Session()
-    PARAMS = {
+
+    PARAMS: dict[str, str] = {
         "action": "query",
         "prop": "revisions",
         "titles": title,
@@ -21,21 +29,20 @@ def make_request(title, rvstart="2023-01-01T00:00:00Z", rvend="2023-09-04T00:00:
         "rvslots": "main",
         "formatversion": "2",
         "format": "json",
-        "rvlimit": 50,
+        "rvlimit": "50",
         "rvend": rvstart,
         "rvstart": rvend
-
     }
+
     response = S.get(URL, headers=HEADERS, params=PARAMS)
     data = response.json()
-    try:
-        revisions = data["query"]["pages"][0]["revisions"]
-    except (KeyError, IndexError):
-        revisions = []
+
+    revisions: list[dict[str, Any]] = data["query"]["pages"][0]["revisions"]
+
     return revisions
 
 
-def get_revisions_data(start_idx, end_idx, checkpoint):
+def get_revisions_data(start_idx: int, end_idx: int, checkpoint: int) -> None:
     """
     Fetches and processes revision data for a range of Wikipedia pages.
 
@@ -52,8 +59,12 @@ def get_revisions_data(start_idx, end_idx, checkpoint):
     Returns:
         None
     """
+    if not Path(DATA_PATH).exists():
+        pd.read_parquet("hf://datasets/euirim/goodwiki/09_04_2023_v1.parquet") \
+           .to_parquet(DATA_PATH)
+
     goodwiki = pl.read_parquet(DATA_PATH)
-    revisions_data = []
+    revisions_data: list[dict[str, Any]] = []
 
     for row in range(start_idx, end_idx):
         title = goodwiki["title"][row]
@@ -62,33 +73,33 @@ def get_revisions_data(start_idx, end_idx, checkpoint):
         revisions = make_request(title)
 
         ### Get all revisions starting 2017-01-01
-        revisions_list = []
+        revisions_list: list[dict[str, Any]] = []
+
         lastFound = False
         while not lastFound:
             if len(revisions) < 50:
                 lastFound = True
+
             for rev in revisions:
                 rev["pageid"] = pageId
                 rev["title"] = title
                 revisions_list.append(rev)
+
             if not lastFound:
                 rvend = revisions[-1]["timestamp"]
+
                 revisions = make_request(title, rvend=rvend)
+    
 
         revisions_data.extend(revisions_list)
         print(f"Processed {row+1}/{len(goodwiki)}: {title} with {len(revisions_list)} revisions.")
+
         if (row + 1) % checkpoint == 0 or (row + 1) == end_idx:
             df = pl.DataFrame(revisions_data)
             df.write_parquet(f"{SAVING_PATH}/revisions_{start_idx}_{end_idx}_{row+1}.parquet")
             print(f"Checkpoint reached at {row+1}. Data saved.")
-            revisions_data = []  # Clear the list after saving
+            revisions_data.clear()  # Clear the list after saving
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch Wikipedia revisions data.")
-    parser.add_argument("--start_idx", type=int, required=True, help="Starting index of pages to process.")
-    parser.add_argument("--end_idx", type=int, required=True, help="Ending index (exclusive) of pages to process.")
-    parser.add_argument("--checkpoint", type=int, default=1000, help="Number of pages to process before saving data.")
-    args = parser.parse_args()
-
-    get_revisions_data(args.start_idx, args.end_idx, args.checkpoint)
+    cli(get_revisions_data)
